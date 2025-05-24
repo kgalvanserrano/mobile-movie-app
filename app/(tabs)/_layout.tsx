5879d06f87b6bd98a5ba83de055fa6dfc7729eba
Kevin <kevin.galvanserrano@gmail.com>
2025-05-23 19:51:09 -0700

    Enhance TabIcon component and tab bar layout for improved UI consistency and usability

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,17 +5,16 @@ import React from "react";
 import { Image, ImageBackground, Text, View } from "react-native";
 
 const TabIcon = ({focused, icon, title}: any) => {
-
     if (focused) {
         return ( // if focused, show highlight
             <ImageBackground
             source={images.highlight}
-            className="flex-row items-center justify-center w-28 h-14 mt-4 rounded-full overflow-hidden"
+            className="flex-row items-center justify-center w-28 h-16 mt-4 rounded-full overflow-hidden" //this is what gives the pill shape
             >
             <Image
                 source={icon}
                 tintColor="#151312"
-                className="w-5 h-5 mr-2"
+                className="w-5 h-5 mr-2" // this is the icon size
             />
             <Text className="text-black font-semibold">{title}</Text>
             </ImageBackground>
@@ -34,7 +33,28 @@ const TabIcon = ({focused, icon, title}: any) => {
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+        screenOptions={{ // this modifies the tab bar to show just icons no labels
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: '100%',
+                height: '100%',
+                justifyContent: 'center',
+                alignItems: 'center',
+            },
+            tabBarStyle: { // this modifies the tab bar to look more rounded, pill designed
+                backgroundColor: '#0f0D23',
+                borderRadius: 50,
+                marginHorizontal: 20,
+                marginBottom: 36,
+                height: 52,
+                position: 'absolute',
+                overflow: 'hidden',
+                borderWidth: 1,
+                borderColor: 'transparent'
+            }
+        }}
+    >
       <Tabs.Screen
         name="index"
         options={{
