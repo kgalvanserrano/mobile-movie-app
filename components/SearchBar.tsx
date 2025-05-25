0a6eb030e85d041f7d6cdf2099ed96c11baaab19
Kevin <kevin.galvanserrano@gmail.com>
2025-05-25 03:55:19 -0700

    Refactor Index and SearchBar components for improved layout and functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,12 @@
+import { icons } from '@/constants/icons'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 
 const SearchBar = () => {
   return (
-    <View>
-      <Text>SearchBar</Text>
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+        <Image source={icons.search} className="size-5" resizeMode='contain' tintColor="#ab8bff" />
+        <Text className="text-white font-semibold ml-3">Searching...</Text>
     </View>
   )
 }
